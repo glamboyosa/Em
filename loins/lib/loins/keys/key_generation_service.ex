47b5f1289e093b7dbe474c26215a3b5b318eb808
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-04-13 13:42:30 +0100

    feat: move logic from depecrated erlang lib

--- loins/lib/loins/keys/key_generation_service.ex
+++ loins/lib/loins/keys/key_generation_service.ex
@@ -3,10 +3,10 @@ defmodule Loins.Keys.KeyGenerationService do
   Generates a crytographically randomly secure string.
   Returns {:ok, key}
   """
-  def gen_keys do
+  def gen_hash do
     symbols = '0123456789abcdefghijklmnopqrstuvwxyz'
     symbol_count = Enum.count(symbols)
-    key = for _ <- 1..6, into: "", do: <<Enum.at(symbols, :crypto.rand_uniform(0, symbol_count))>>
+    key = for _ <- 1..6, into: "", do: <<Enum.at(symbols, :rand.uniform(symbol_count))>>
     {:ok, key}
   end
 end
