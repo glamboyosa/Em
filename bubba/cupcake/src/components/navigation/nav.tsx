98e56601e68756058c251c300d6d05e3a52669ca
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-04-19 12:06:19 +0100

    feat: add Github link

--- bubba/cupcake/src/components/navigation/nav.tsx
+++ bubba/cupcake/src/components/navigation/nav.tsx
@@ -7,20 +7,31 @@ const Nav = ({ style }: NavProps) => {
   return (
     <nav className={styles.nav}>
       <ArrowLeftIcon className={styles.arrow} />
-      <Avatar style={style} className={styles.avatar}>
-        <Popover>
-          <Trigger asChild>
-            <AvatarFallback className={styles.avatarFallback}>
-              TO
-            </AvatarFallback>
-          </Trigger>
-          <Content className={styles.flex}>
-            <div>Analytics 📈</div>
-            <div>Custom Links 🔗</div>
-            <div>Log out 🙁</div>
-          </Content>
-        </Popover>
-      </Avatar>
+      <span className={styles.navRHS}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="http://github.com/glamboyosa/em"
+          style={{ textDecoration: 'underline', color: 'inherit' }}
+        >
+          Github
+        </a>
+
+        <Avatar style={style} className={styles.avatar}>
+          <Popover>
+            <Trigger asChild>
+              <AvatarFallback className={styles.avatarFallback}>
+                TO
+              </AvatarFallback>
+            </Trigger>
+            <Content className={styles.flex}>
+              <div>Analytics 📈</div>
+              <div>Custom Links 🔗</div>
+              <div>Log out 🙁</div>
+            </Content>
+          </Popover>
+        </Avatar>
+      </span>
     </nav>
   )
 }
